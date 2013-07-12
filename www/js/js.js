689e09fe6e66ec418ff325c89564f1c66c6a49de
buzz69 <sebastien.masson.pro@gmail.com>
2013-07-12 10:44:44 +0200

    Test 3: snapshot en boucle

--- www/js/js.js
+++ www/js/js.js
@@ -1,4 +1,23 @@
-		$( document ).bind( "mobileinit", function() {  
-				// Make your jQuery Mobile framework configuration changes here!  
-				$.mobile.allowCrossDomainPages = true;
-		});
+	var snapshotUrl='http://lonsdale.dyndns.org:1080/snapshot.cgi?user=admin&pwd=lezennes';
+	
+	$( document ).bind( "mobileinit", function() {  
+		// Make your jQuery Mobile framework configuration changes here!  
+		$.mobile.allowCrossDomainPages = true;
+	});
+
+	$( '#viewPage' ).live( 'pageshow',function(event){
+		$('#viewImg').attr('src',snapshotUrl);
+		motion();
+	});
+	
+	function motion(){
+		randomNum=Date.now();
+		tmpUrl=snapshotUrl+'&time='+randomNum;
+		tmpIMG=new Image();
+		tmpIMG.src=tmpUrl;
+		tmpIMG.onload = function(){
+			$('#viewImg').attr('src',tmpUrl);
+			//setTimeout("motion()",500);
+			motion();
+		}
+	}
