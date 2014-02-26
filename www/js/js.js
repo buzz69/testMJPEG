d5e7879963b32a6959ed8775e87c22681145ee82
buzz69 <sebastien.masson.pro@gmail.com>
2014-02-26 22:36:46 +0100

    modif to canva

--- www/js/js.js
+++ www/js/js.js
@@ -1,4 +1,6 @@
-	var snapshotUrl='http://lonsdale.dyndns.org:1080/snapshot.cgi?user=admin&pwd=lezennes';
+	var flux='http://lonsdale.dyndns.org:1080/snapshot.cgi?user=admin&pwd=lezennes';
+	var ctx=null;
+	var motionInterval=1000;	//délai d'affichage entre les images de la caméra
 	
 	$( document ).bind( "mobileinit", function() {  
 		// Make your jQuery Mobile framework configuration changes here!  
@@ -6,12 +8,14 @@
 	});
 
 	$( '#viewPage' ).live( 'pageshow',function(event){
-		$('#viewImg').attr('src',snapshotUrl);
+		//$('#viewImg').attr('src',snapshotUrl);
 		//$('#viewImg').attr('width',$(window).width()+'px');
 		navigator.notification.alert("Start motion");
+		ctx = document.getElementById('viewImg').getContext('2d');
 		motion();
 	});
 	
+	/*
 	function motion(){
 		randomNum=Date.now();
 		tmpUrl=snapshotUrl+'&time='+randomNum;
@@ -22,4 +26,27 @@
 			//setTimeout("motion()",500);
 			motion();
 		}
+	}
+	*/
+	function motion(){
+			randomNum=Date.now();
+			tmpUrl=flux+'&time='+randomNum;
+			tmpIMG=new Image();
+			tmpIMG.src=tmpUrl;
+			//console.log("load image: ("+flux+")");
+			tmpIMG.onload= function(){
+				//console.log("image loaded: ("+tmpIMG.src+")");
+					ratio=this.width/this.height;
+					largeur=Math.ceil(window.innerWidth-(window.innerWidth/10));	//largeur ecran -10%
+					hauteur=Math.ceil(largeur/ratio);
+					$('#viewImg').attr('width',largeur);
+					$('#viewImg').attr('height',hauteur);
+					//console.log('source: '+this.width+'x'+this.height+' - destination: '+largeur+'x'+hauteur);
+					ctx.drawImage(tmpIMG, 0, 0, this.width, this.height, 0, 0, largeur, hauteur);
+					setTimeout("motion()",motionInterval);
+			};
+			tmpIMG.onerror=function(){
+				//console.log("load error: ("+tmpIMG.src+")");
+				motion();
+			};
 	}
