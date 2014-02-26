c59631149d4d9930567be5faf99d5536fc488bfc
buzz69 <sebastien.masson.pro@gmail.com>
2014-02-26 22:49:53 +0100

    Retrait du redimensionnement

--- www/js/js.js
+++ www/js/js.js
@@ -12,6 +12,7 @@
 		$('#infos').append("Page loaded<br>");
 		ctx = document.getElementById('viewImg').getContext('2d');
 		$('#infos').append("Canva registred<br>");
+		$('#infos').append("Start motion: "+flux+"<br>");
 		motion();
 	});
 	
@@ -29,27 +30,22 @@
 	}
 	*/
 	function motion(){
-		$('#infos').append("Start motion: "+flux+"<br>");
 			randomNum=Date.now();
 			tmpUrl=flux+'&time='+randomNum;
 			tmpIMG=new Image();
 			tmpIMG.src=tmpUrl;
-			//console.log("load image: ("+flux+")");
 			tmpIMG.onload= function(){
-				$('#infos').append("Picture loaded !<br>");
-				//console.log("image loaded: ("+tmpIMG.src+")");
-					ratio=this.width/this.height;
-					largeur=Math.ceil(window.innerWidth-(window.innerWidth/10));	//largeur ecran -10%
-					hauteur=Math.ceil(largeur/ratio);
-					$('#viewImg').attr('width',largeur);
-					$('#viewImg').attr('height',hauteur);
-					//console.log('source: '+this.width+'x'+this.height+' - destination: '+largeur+'x'+hauteur);
-					$('#infos').append("Drawing canva<br>");
-					ctx.drawImage(tmpIMG, 0, 0, this.width, this.height, 0, 0, largeur, hauteur);
+				//$('#infos').append("Picture loaded !<br>");
+					//ratio=this.width/this.height;
+					//largeur=Math.ceil(window.innerWidth-(window.innerWidth/10));	//largeur ecran -10%
+					//hauteur=Math.ceil(largeur/ratio);
+					//$('#viewImg').attr('width',largeur);
+					//$('#viewImg').attr('height',hauteur);
+					//$('#infos').append("Drawing canva<br>");
+					ctx.drawImage(tmpIMG, 0, 0, this.width, this.height, 0, 0, 320, 240);
 					setTimeout("motion()",motionInterval);
 			};
 			tmpIMG.onerror=function(){
-				//console.log("load error: ("+tmpIMG.src+")");
 				$('#infos').append("Picture load failed !<br>");
 				motion();
 			};
